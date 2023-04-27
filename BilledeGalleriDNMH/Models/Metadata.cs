ed50f6d089d4519885f499faff2f087e89a27f77
Anders Hansen <58811906+adidas420x@users.noreply.github.com>
2023-04-27 09:11:58 +0200

    Scaffolded API and Models and made model objects

---
+++ BilledeGalleriDNMH/Models/Metadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Metadata
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public string CameraInformation { get; set; }
+        public string CopyrightInformation { get; set; }
+        public List<string> Keywords { get; set; }
+    }
+}
