38b154c410a959bca725f181f81798a9278617ca
HyggenHD <75610123+HyggenHD@users.noreply.github.com>
2023-05-01 17:08:30 +0200

    .

--- BilledeGalleriDNMH/MongoDBRepository/Repositories/ImageMetadataRepository.cs
+++ BilledeGalleriDNMH/MongoDBRepository/Repositories/ImageMetadataRepository.cs
@@ -37,17 +37,6 @@ namespace MongoDBRepository.Repositories
             return result.DeletedCount != 0;
         }
 
-        public async Task<List<ImageMetadata>> ReadMany(BaseQueryParameters? filterObject = null)
-        {
-            FilterDefinition<ImageMetadata> filter = CreateFilterDefinition(filterObject);
-
-            SortDefinition<ImageMetadata> sort = CreateSortDefinition(filterObject);
-
-            List<ImageMetadata> list = await collection.Find(filter).Sort(sort).ToListAsync();
-
-            return list;
-        }
-
         public async Task<List<ImageMetadata>> FilterImageMetadata(ImageMetadataQueryParameters queryParameters)
         {
 
@@ -126,63 +115,5 @@ namespace MongoDBRepository.Repositories
             return sort;
         }
 
-        public FilterDefinition<ImageMetadata> CreateFilterDefinition(BaseQueryParameters? filterObject)
-        {
-            FilterDefinitionBuilder<ImageMetadata> filterBuilder = Builders<ImageMetadata>.Filter;
-
-            FilterDefinition<ImageMetadata> filter = filterBuilder.Empty;
-
-            if (filterObject != null && filterObject is ImageMetadataQueryParameters)
-            {
-                ImageMetadataQueryParameters queryParameters = (ImageMetadataQueryParameters)filterObject;
-
-                if (queryParameters.Id != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Id, queryParameters.Id);
-                }
-
-                if (queryParameters.Image != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Image, queryParameters.Image);
-                }
-
-                if (queryParameters.Title != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Title, queryParameters.Title);
-                }
-
-                if (queryParameters.Description != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Description, queryParameters.Description);
-                }
-
-                if (queryParameters.DateTime != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.DateTime, queryParameters.DateTime);
-                }
-
-                if (queryParameters.Location != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Location, queryParameters.Location);
-                }
-
-                if (queryParameters.CameraInformation != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.CameraInformation, queryParameters.CameraInformation);
-                }
-
-                if (queryParameters.CopyrightInformation != null)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.CopyrightInformation, queryParameters.CopyrightInformation);
-                }
-
-                if (queryParameters.Keywords.Length != 0)
-                {
-                    filter &= filterBuilder.Eq(imageMetadata => imageMetadata.Keywords, queryParameters.Keywords);
-                }
-            }
-
-            return filter;
-        }
     }
 }
