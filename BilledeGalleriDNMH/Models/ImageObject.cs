43e3dd6361ddc822f715c791b8803ed1e71a6ff6
HyggenHD <75610123+HyggenHD@users.noreply.github.com>
2023-05-08 13:43:52 +0200

    Fixed upload image. and made progress towards making the gallary work we need to fix                     var documents = BsonSerializer.Deserialize<List<ImageMetadata>>(content);

---
+++ BilledeGalleriDNMH/Models/ImageObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ImageObject
+    {
+        public byte[] Data { get; set; }
+        public string Base64 { get; set; }
+        public System.Drawing.Image Object { get; set; }
+    }
+}
--- BilledeGalleriDNMH/WebApp/Controllers/HomeController.cs
+++ BilledeGalleriDNMH/WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Models;
 using System.Diagnostics;
 using WebApp.Models;
 using WebApp.Service;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WebApp.Controllers
 {
@@ -50,22 +51,78 @@ namespace WebApp.Controllers
                     await imageMetadataService.UploadImage(file);
                 }
             }
-            return View("Succes");
+            return View("index");
         }
 
-        //public ActionResult Gallery()
-        //{
-        //    string[] filenames = Directory.GetFiles(Server.MapPath("~/images"));
-        //    List<string> imageUrls = new List<string>();
-        //    foreach (string filename in filenames)
-        //    {
-        //        string imageUrl = Url.Content("~/images/" + Path.GetFileName(filename));
-        //        imageUrls.Add(imageUrl);
-        //    }
+        [HttpGet]
+        [Route("[controller]/Gallery")]
+        public async Task<IActionResult> Images(string title, string description, string dateTime, string location, string cameraInformation, string copyrightInformation, string keywords)
+        {
+            ImageMetadataService metadataService = new();
 
-        //    ViewBag.ImageUrls = imageUrls;
-        //    return View();
-        //}
+            string[] keywordArray = ConvertKeywordsToArray(keywords);
+            DateTime? dateTimeValue = ConvertStringToDateTime(dateTime);
+
+            List<ImageMetadata> images = await metadataService.GetImageMetadata(title, description, dateTimeValue, location, cameraInformation, copyrightInformation, keywordArray);
+
+            List<ImageObject> imageObjects = new List<ImageObject>();
+
+            foreach (var image in images)
+            {
+                imageObjects.Add(ConvertBytesToImage(image.Image));
+            }
+
+            return View(imageObjects);
+        }
+        private ImageObject ConvertBytesToImage(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+            else
+            {
+                using (var ms = new MemoryStream(data))
+                {
+                    var image = System.Drawing.Image.FromStream(ms);
+
+                    return new ImageObject
+                    {
+                        Data = data,
+                        Base64 = Convert.ToBase64String(data),
+                        Object = image
+                    };
+                }
+            }
+        }
+
+        private string[] ConvertKeywordsToArray(string keywords)
+        {
+            if (string.IsNullOrWhiteSpace(keywords))
+            {
+                return new string[0];
+            }
+            else
+            {
+                return keywords.Split(',').Select(k => k.Trim()).ToArray();
+            }
+        }
+
+        private DateTime? ConvertStringToDateTime(string dateTimeString)
+        {
+            if (string.IsNullOrWhiteSpace(dateTimeString))
+            {
+                return null;
+            }
+            else if (DateTime.TryParse(dateTimeString, out DateTime dateTime))
+            {
+                return dateTime;
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
