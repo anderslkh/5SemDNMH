a1b2ddb5ca919c634557bfc74f2bd11e68fd0757
Anders Hansen <58811906+adidas420x@users.noreply.github.com>
2023-05-25 20:25:06 +0200

    Added button and input field for creation of gallery, added createGallery method in galleryController

---
+++ BilledeGalleriDNMH/WebApp/Controllers/GalleryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using WebApp.Service;
+
+namespace WebApp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly ILogger<GalleryController> _logger;
+        private readonly GalleryService _galleryService;
+
+        public GalleryController(
+            ILogger<GalleryController> logger,
+            GalleryService galleryService)
+        {
+            _logger = logger;
+            _galleryService = galleryService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[controller]CreateGallery")]
+        public async Task<ActionResult> CreateGallery(string name, List<ImageObject> imageObjects)
+        {
+
+            if (name != null && imageObjects != null) 
+            {
+                await _galleryService.CreateGallery(name, imageObjects);
+            }
+
+            return View("index");
+        }
+    }
+}
